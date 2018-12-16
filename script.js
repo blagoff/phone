297eaffc30f7028196df010a6e1c66ff5a51d700
Przemyslaw Przewoznik <blagoff88@gmail.com>
2018-12-16 17:56:12 +0100

    fix

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ function Phone(brand, price, color, system)
 	this.price = price;
 	this.color = color;
 	this.system = system;
-
+}
 	Phone.prototype.printInfo = function(){
 		console.log("The phone brand is " + this.brand + ", color is " + this.color + " price is " + this.price + ".");
-
+	}
+	
 	Phone.prototype.showOS = function(){
 		alert("Operating system is " + this.system + ".");
 	}
 
-	}
-}
+	
+
 
 var iPhone6S = new Phone("Apple",2250,"silver","iOS");
 var GalaxyS6 = new Phone("Samsung",500,"black", "android");
